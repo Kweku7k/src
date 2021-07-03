f19ea868be725784b421f775321e9a45aaceaf92
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2021-07-03 12:49:08 +0000

    up

--- app.py
+++ app.py
@@ -1,13 +1,161 @@
 from flask import Flask,redirect,url_for,render_template,request
+from flask.helpers import flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from forms import *
+# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+from PIL import Image
+import os
 
 app=Flask(__name__)
+db = SQLAlchemy(app)
+
+# UPLOAD_FOLDER = 'static/img/uploads'
+# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['SECRET_KEY'] = 'll91628bb0b13ce0c676d32e2vsba245'
+app.config['UPLOADED_IMAGES_DEST'] = 'static/uploads/images'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
+
+# Takes the name of the file and the extensions
+images = UploadSet('images', IMAGES)
+configure_uploads(app, images)
+
+class Posts(db.Model):
+    tablename = ['Posts']
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author = db.Column(db.String)
+    image_file = db.Column(db.String(200), default='default.png')
+    
+    def __repr__(self):
+        return f"Posts('{self.id}', '{self.title}')"
+
+
+class Issue(db.Model):
+    tablename = ['Posts']
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author = db.Column(db.String)
+    
+    def __repr__(self):
+        return f"Posts('{self.id}', '{self.title}')"
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
+    limitpost = Posts.query.order_by(Posts.id.desc()).limit(3).all()
+    print("The Posts" + str(limitpost)) 
+    return render_template('index.html', limitpost=limitpost)
 
+@app.route("/post/<int:id>")
+def post(id):
+    post = Posts.query.get_or_404(id)
+    print(post)
+    return render_template('post.html', post=post)
+
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    post = Posts.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'Your post has been deleted','danger')
+    print(post)
+    return redirect(url_for('adminPosts'))
+
+
+@app.route("/edit/<int:post_id>", methods=['GET','POST'])
+def edit(post_id):
+    form = AddPostForm()
+    post = Posts.query.get_or_404(post_id)
+    print(post.id)
+    if request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        form.author.data = post.author
+    elif request.method == 'POST':
+        print("Post ")
+        if form.validate_on_submit(): 
+            post.title = form.title.data
+            post.content = form.content.data
+            db.session.commit()
+            flash(f'Your post has been editted succesfully','success')
+            return redirect(url_for('adminPosts'))
+            print(post)
+    return render_template('editpost.html', form=form, title=post.id, post=post)
+
+@app.route('/updates')
+def updates(): 
+    return render_template('updates.html')
+
+
+@app.route('/admin')
+def admin():    
+    return render_template('admin.html')
+
+
+@app.route("/profile/<int:id>")
+def profile(id):
+    return render_template('profile.html')
+
+@app.route("/update/<int:id>")
+def update(id):
+    return render_template('update.html')
+
+@app.route("/feedback")
+def feedback():
+    return render_template('feedback.html')
+
+@app.route("/admin/posts")
+def adminPosts():
+    posts = Posts.query.all()
+    return render_template('posts.html', posts=posts)
+
+@app.route("/addpost", methods=['GET','POST'])
+def addpost():
+    form = AddPostForm()
+    if form.validate_on_submit():
+        # print(form.picture.data)
+        if form.picture.data:
+            filename = images.save(form.picture.data)
+        else:
+            filename = 'IneruuTrade.png'
+        print(filename)
+        newPost = Posts(title=form.title.data, content=form.content.data, author=form.author.data, image_file = filename)
+        db.session.add(newPost)
+        db.session.commit()
+        print("Form has been submitted successfully")
+        flash(f'Your post has been uploaded successfully', 'success')
+
+        return redirect(url_for('admin'))
+    return render_template('addpost.html', form=form)
+
+
+@app.route("/raiseissue", methods=['GET','POST'])
+def raiseissue():
+    form = RaiseIssue()
+    if form.validate_on_submit():
+        newIssue = Issue(title=form.title.data, content=form.content.data, author=form.author.data, image_file=form.picture.data)
+        db.session.add(newIssue)
+        db.session.commit()
+        print("Form has been submitted successfully")
+        flash(f'Your post has been uploaded successfully', 'success')
+
+        return redirect(url_for('admin'))
+    return render_template('addpost.html', form=form)
+
+@app.route("/team")
+def team():
+    return render_template('team.html')
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
---
+++ forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.widgets import TextArea
+
+class AddPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+    picture = FileField('Add a picture', validators=[ FileAllowed(['jpg', 'png','jpeg'])])
+    content = StringField('Description', widget=TextArea(), validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=15, message="Your phone number should be more than 10 digits and less than 15")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Your passwords dont match, please try again')])
+    submit = SubmitField('Sign Up')
+
+    # def validate_phone(self, phone):
+    #     user = User.query.filter_by(phone=phone.data).first()
+    #     if user:
+    #         raise ValidationError('That phone number is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    phone = StringField('Phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
